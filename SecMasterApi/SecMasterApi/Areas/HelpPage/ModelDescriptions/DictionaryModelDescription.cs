feb734983fe9c3c24cf0d196590585bf163baa5a
Saurabh Rajendra Borawake <srborawake@ivp.co.in>
2023-08-11 19:20:14 +0530

    added some apis

---
+++ SecMasterApi/SecMasterApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SecMasterApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SecMasterApi/SecMasterApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecMasterApi.WebApiApplication" Language="C#" %>
